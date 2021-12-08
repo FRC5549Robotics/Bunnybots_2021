a9f29266e220b550cd91ec84096016b110559d28
Shaunak <shaunakduude@gmail.com>
2021-12-08 10:56:47 -0500

    Update IntakeForward.java

--- src/main/java/frc/robot/commands/IntakeForward.java
+++ src/main/java/frc/robot/commands/IntakeForward.java
@@ -34,7 +34,7 @@ public class IntakeForward extends CommandBase {
     }
   } */
   m_intake.forward();
-
+  }
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
