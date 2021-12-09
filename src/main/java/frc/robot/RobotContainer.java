044ce41fe77723d95d7b8beea4adc316bf4f0d17
Dylan Nguyen <dylannguyen1000@gmail.com>
2021-12-09 17:57:11 -0500

    revert: TriggerActive class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,8 +41,8 @@ public class RobotContainer {
   /* Xbox Controller */
   public static XboxController xbox =  new XboxController(Constants.XBOX_CONTROLLER);
 
-  // public static double intakebutton = xbox.getRawAxis(Constants.INTAKE_AXIS);
-  public static TriggerActive intakebutton = new TriggerActive(xbox, Constants.INTAKE_AXIS, Constants.XBOX_TOLERANCE);
+  public static double intakeButton = xbox.getRawAxis(Constants.INTAKE_AXIS);
+  // public static TriggerActive intakeButton = new TriggerActive(xbox, Constants.INTAKE_AXIS, Constants.XBOX_TOLERANCE);
   public static JoystickButton liftUpButton = new JoystickButton(xbox, Constants.LIFTUP_AXIS);
   public static JoystickButton liftDownButton = new JoystickButton(xbox, Constants.LIFTDOWN_AXIS);
   
@@ -61,8 +61,8 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     drivetrain.setDefaultCommand(new TankDrive(drivetrain, joystickLeftAxis, joystickRightAxis, joystickRightRotate));
-    // intake.setDefaultCommand(new IntakeForward(intake, intakebutton));
-    intakebutton.whenActive(new IntakeForward(intake));
+    intake.setDefaultCommand(new IntakeForward(intake, intakeButton));
+    // intakeButton.whenActive(new IntakeForward(intake));
     liftUpButton.whenPressed(new LiftUp(lift));
     liftDownButton.whenPressed(new LiftDown(lift));
   }
