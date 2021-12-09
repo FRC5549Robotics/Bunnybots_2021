fd8967d3fd57ac03a3a84ca0600e1339051efa32
Shaunak <shaunakduude@gmail.com>
2021-12-09 16:14:34 -0500

    Update TriggerTolerance.java

--- src/main/java/frc/robot/subclass/TriggerTolerance.java
+++ src/main/java/frc/robot/subclass/TriggerTolerance.java
@@ -1,18 +1,21 @@
+package frc.robot.subclass;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 public class TriggerActive extends JoystickButton {
-  private final GenericHID m_joystick;
-  private final int m_axisNumber;
-  private final double m_tolerance;
-
-  public JoystickButton(GenericHID joystick, int buttonNumber, double tolerance) {
-    requireNonNullParam(joystick, "joystick", "JoystickButton");
-
-    m_joystick = joystick;
-    m_axisNumber = axisNumber;
-    m_tolerance = tolerance;
+    private final GenericHID m_joystick;
+    private final int m_axisNumber;
+    private final double m_tolerance;
+  
+    public TriggerActive(GenericHID joystick, int buttonNumber, double tolerance) {
+  
+      m_joystick = joystick;
+      m_axisNumber = buttonNumber;
+      m_tolerance = tolerance;
+    }
+  
+    public boolean get() {
+      return m_joystick.getRawAxis(m_axisNumber) > m_tolerance;
+    }
   }
-
-  public boolean get() {
-    return m_joystick.getRawAxis(m_axisNumber) > m_tolerance;
-  }
-}
-isActive
