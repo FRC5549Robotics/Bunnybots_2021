7f770ee8f5f0d62cd3a3370dc254a55554a622a4
Shaunak <shaunakduude@gmail.com>
2021-12-17 18:11:07 -0500

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-
+import edu.wpi.first.wpilibj.XboxController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Constants;
 
@@ -10,16 +10,17 @@ public class Intake extends SubsystemBase {
   /* Intake Class */
   WPI_TalonSRX motorIntakeLeft, motorIntakeRight;
   SpeedControllerGroup motorIntakeGroup;
+
+  public XboxController xboxTrigger = new XboxController(0);
   
   public Intake() {
     /* Creates a new Intake. */
     motorIntakeLeft = new WPI_TalonSRX(Constants.MOTOR_INTAKE_LEFT);
     motorIntakeRight = new WPI_TalonSRX(Constants.MOTOR_INTAKE_RIGHT);
-
+    motorIntakeLeft.setInverted(true);
     // turns right motor in opposite direction of left motor
     // allows for motors to rotate towards the center, intaking objects
     // or rotating outwards from the center, ejecting objects
-    motorIntakeRight.setInverted(true);
 
     motorIntakeGroup = new SpeedControllerGroup(motorIntakeLeft, motorIntakeRight);
   }
@@ -39,5 +40,20 @@ public class Intake extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if(xboxTrigger.getRawAxis(5) > 0.25) {
+      motorIntakeRight.set(Constants.INTAKE_SPEED_OUT);
+    } else if (xboxTrigger.getRawAxis(5) < -0.25) {
+      motorIntakeRight.set(-Constants.INTAKE_SPEED);
+    } else {
+      motorIntakeRight.set(0);
+    }
+
+    if(xboxTrigger.getRawAxis(1) > 0.25){
+      motorIntakeLeft.set(Constants.INTAKE_SPEED_OUT);
+    } else if (xboxTrigger.getRawAxis(1) < -0.25) {
+      motorIntakeLeft.set(-Constants.INTAKE_SPEED);
+    } else {
+      motorIntakeLeft.set(0);
+    }
   }
 }
