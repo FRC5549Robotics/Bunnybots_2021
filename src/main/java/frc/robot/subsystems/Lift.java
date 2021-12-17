a4017237e6879f1d8bcde1c7c05051396b891806
Shaunak <shaunakduude@gmail.com>
2021-12-17 18:10:35 -0500

    Update Lift.java

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -3,10 +3,14 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.XboxController;
+
 
 public class Lift extends SubsystemBase {
   /* Lift Class */
   WPI_TalonSRX LiftMotor;
+  public XboxController xboxTrigger = new XboxController(0);
+
 
   public Lift() {
     /* Creates a new Lift. */
@@ -23,5 +27,21 @@ public class Lift extends SubsystemBase {
 
   public void off() {
     LiftMotor.set(0);
+  }
+  @Override
+  public void periodic() {
+    if(xboxTrigger.getRawAxis(2) > 0.5)
+    {
+      LiftMotor.set(Constants.LIFT_SPEED);
+    }
+    else if(xboxTrigger.getRawAxis(3) > 0.5)
+    {
+      LiftMotor.set(-Constants.LIFT_SPEED);
+    }
+    else
+    {
+      LiftMotor.set(0);
+    }
+    
   }
 }
